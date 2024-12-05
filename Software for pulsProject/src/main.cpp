6db9145bb89cb7cb5c73c77437e911d4125e8ebd
Creciunel <60750488+Creciunel@users.noreply.github.com>
2024-12-05 22:43:55 +0200

    Update teori

--- Software for pulsProject/src/main.cpp
+++ Software for pulsProject/src/main.cpp
@@ -1,31 +1,54 @@
+#include <driver/dac.h>
 #include <Arduino.h>
-#include <GyverMAX6675.h>
-
-// Definirea pinilor pentru MAX6675
-#define CLK_PIN 14  // Pinul pentru CLK
-#define DATA_PIN 12 // Pinul pentru DATA
-#define CS_PIN 15   // Pinul pentru CS
-
-// Crearea instanței pentru senzorul MAX6675
-GyverMAX6675<CLK_PIN, DATA_PIN, CS_PIN> sens;
 
 void setup() {
-    Serial.begin(115200); // Inițializarea comunicației seriale
-    delay(1000); // Așteaptă 1 secundă pentru a permite conectarea serialului
-
-    Serial.println("Testare termocuplu MAX6675...");
+    Serial.begin(115200);
+    // Activează DAC Channel 1 (GPIO25)
+    dac_output_enable(DAC_CHANNEL_1);
 }
 
 void loop() {
-    // Citirea temperaturii
-    if (sens.readTemp()) { // Verifică dacă citirea a avut succes
-        float temperature = sens.getTemp(); // Obține temperatura
-        Serial.print("Temperatura: ");
-        Serial.print(temperature);
-        Serial.println(" °C"); // Afișează temperatura în grade Celsius
+    static float voltage = 0.0;
+    static bool firstIteration = true; // Flag pentru prima iterație
+
+    if (firstIteration) {
+        // Crește cu 50 mV doar la prima iterație
+        voltage += 0.05;
+        Serial.println("Prima iterație: Creștere cu 50 mV");
+        firstIteration = false; // Schimbă flag-ul
     } else {
-        Serial.println("Eroare la citirea temperaturii!");
+        // Crește cu 60 mV
+        voltage += 0.09;
+        Serial.println("Creștere cu 60 mV");
     }
 
-    delay(1000); // Așteaptă 1 secundă înainte de următoarea citire
+    // Asigură-te că tensiunea nu depășește 3.3V
+    if (voltage > 3.3) {
+        voltage = 0;
+        Serial.println("Tensiunea a atins maximul de 3.3V");
+    }
+
+    // Setează tensiunea DAC-ului
+    dac_output_voltage(DAC_CHANNEL_1, (uint8_t)(voltage * 255 / 3.3));
+    Serial.print("Tensiune curentă: ");
+    Serial.print(voltage, 2); // Afișează cu două zecimale
+    Serial.println(" V");
+    delay(10); // Așteaptă 100 ms
+
+    // Scade cu 30 mV
+    voltage -= 0.03;
+    
+    // Asigură-te că tensiunea nu devine mai mică de 0V
+    if (voltage < 0) {
+        voltage = 0;
+        Serial.println("Tensiunea a atins minimul de 0V");
+    }
+
+    // Setează tensiunea DAC-ului
+    dac_output_voltage(DAC_CHANNEL_1, (uint8_t)(voltage * 255 / 3.3));
+    Serial.print("Tensiune curentă după scădere: ");
+    Serial.print(voltage, 2); // Afișează cu două zecimale
+    Serial.println(" V");
+    delay(20); // Așteaptă 200 ms
+
 }
