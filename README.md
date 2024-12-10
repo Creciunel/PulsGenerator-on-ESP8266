f19abb0c75f3b5be90ca16fb51cacdb61c87b30e
Catalin Creciunel <creciunelcatalin@gmail.com>
2024-12-10 12:41:10 +0200

    Add CI/CD

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# PulsGenerator-on-ESP8266
-A complicated signal generator based on ESP8266
+# PulsGenerator-ESP32
+
+Generate diferent form of signal, like square, triangle, sawtooth, etc. using DAc of ESP32. Code written in c with zephyr framework.
+
+Used cases:
+- Generate signal for potentiostat
+- Generate signal for oscilloscope
+
+![Potentiostat](IMG/Electrode%20schematic.png)
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # source ~/zephyrproject/.venv/bin/activate
 # source ~/zephyrproject/zephyr/zephyr-env.sh
 # ZEPHYR_TOOLCHAIN_VARIANT="zephyr"
-west build -p always -b esp32_devkitc_wroom/esp32/procpu hello_world
+west build -p always -b esp32_devkitc_wroom/esp32/procpu GeneratorCode
 # west flash
 # access COM port from WSL https://docs.espressif.com/projects/vscode-esp-idf-extension/en/latest/additionalfeatures/wsl.html
